fa4f6c965bfe8b6dd94dabb2d6b8661e569ca270
msatlan <markosatlan@gmail.com>
2018-10-19 08:47:53 +0200

    Initial project setup

---
+++ ConversionApp/ConversionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConversionApp
+//
+//  Created by Marko Satlan on 19/10/2018.
+//  Copyright © 2018 Marko Satlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
